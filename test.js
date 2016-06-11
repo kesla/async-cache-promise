2b3a085b642f48a4a9366d52c76995a4a3c6a997
David Björklund <david.bjorklund@gmail.com>
2016-06-11 15:32:01 +0200

    rewrite to extend class

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 import test from 'tapava';
-import setupAsyncCache from './lib';
+import AsyncCache from './lib';
 import Promise from 'bluebird';
 
-test('', function * (t) {
+test('AsyncCache', function * (t) {
   let count = 0;
-  const cache = setupAsyncCache({
+  const cache = new AsyncCache({
     load: key => {
       count++;
       return Promise.resolve(key);
@@ -17,4 +17,7 @@ test('', function * (t) {
   t.is(count, 1);
   t.is(yield cache.get(234), 234);
   t.is(count, 2);
+  cache.set(345, 345);
+  t.is(count, 2);
+  t.is(yield cache.get(345), 345);
 });
