abe4b848e0c371001a4883ae192f2baee01fc708
David Bjorklund <david.bjorklund@gmail.com>
2016-07-06 00:59:01 +0200

    bugfix

--- test.js
+++ test.js
@@ -21,3 +21,35 @@ test('AsyncCache', function * (t) {
   t.is(count, 2);
   t.is(yield cache.get(345), 345);
 });
+
+test('AsyncCache, using promises', t => {
+  let count = 0;
+  const cache = new AsyncCache({
+    load: key => {
+      count++;
+      return Promise.resolve(key);
+    }
+  });
+
+  return cache.get(123)
+    .then(res => {
+      t.is(res, 123);
+      t.is(count, 1);
+      return cache.get(123);
+    })
+    .then(res => {
+      t.is(res, 123);
+      t.is(count, 1);
+      return cache.get(234);
+    })
+    .then(res => {
+      t.is(res, 234);
+      t.is(count, 2);
+      cache.set(345, 345);
+      return cache.get(345);
+    })
+    .then(res => {
+      t.is(res, 345);
+      t.is(count, 2);
+    });
+});
